d44e7f80454ffb0de052e79eff6e1cff4e28f7a9
wafiben <wafibenjeddou@gmail.com>
2022-11-20 19:09:28 +0100

    delete

--- front/src/Components/CardUser.js
+++ front/src/Components/CardUser.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import axios from "axios";
 
-function CardUser({user}) {
-    const handleDelete=()=> {
-        alert()
+function CardUser({ user }) {
+  const url = `http://localhost:9000/api/user/`;
+  const handleDelete = () => {
+    if (window.confirm("are you sure you want to delete this user")) {
+      axios
+        .delete(`${url}${user._id}`)
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
+    window.location.reload();
+  };
   return (
-    <Card style={{ width: '18rem' }}>
-    <Card.Body>
-      <Card.Title>{user.userName}</Card.Title>
-      <Card.Text>
-      {user.email}
-      </Card.Text>
-      <Card.Text>
-      {user.age}
-      </Card.Text>
-      <Button variant="danger" onClick={handleDelete}>deleted</Button>
-    </Card.Body>
-  </Card>
-  )
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Title>{user.userName}</Card.Title>
+        <Card.Text>{user.email}</Card.Text>
+        <Card.Text>{user.age}</Card.Text>
+        <Button variant="danger" onClick={handleDelete}>
+          deleted
+        </Button>
+      </Card.Body>
+    </Card>
+  );
 }
 
-export default CardUser
+export default CardUser;
